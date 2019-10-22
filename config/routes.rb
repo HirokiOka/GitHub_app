2be13c87bd76871cd10448048bfd760d5cf3d064
hiroki <hiro_bass.0827@icloud.com>
2019-10-22 11:37:33 +0900

    UserRegistration added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post 'users/create' => 'users#create'
+  get 'users/index' => 'users#index'
+  get 'users/:id' => 'users#show'
+  get 'user/create' => 'user#create'
+  get '/signup' => 'users#new'
+
+  get '/' => 'home#top'
+  get '/about' => 'home#about'
 end
