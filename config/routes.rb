5237c7bea67710b02a70e1b631d82be8218d555a
hiroki <hiro_bass.0827@icloud.com>
2019-10-22 11:57:06 +0900

    UserEdit added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   post 'users/create' => 'users#create'
+  post 'users/:id/update' => 'users#update'
   get 'users/index' => 'users#index'
   get 'users/:id' => 'users#show'
   get 'user/create' => 'user#create'
   get '/signup' => 'users#new'
+  get 'users/:id/edit' => 'users#edit'
 
   get '/' => 'home#top'
   get '/about' => 'home#about'
