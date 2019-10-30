88612383cb37b735c3939cdba2fcf27da0050352
hiroki <hiro_bass.0827@icloud.com>
2019-10-30 10:38:01 +0900

    quiz added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
   before_action :forbid_login_user, {only: [:top]}
   before_action :forbid_login_user, {only: [:new, :create, :login_form, :login]}
 
+  @@lang = ''
+
   def top
   end
 
@@ -9,14 +11,28 @@ class HomeController < ApplicationController
   end
 
   def quiz
+    require 'net/http'
+    require 'uri'
+    require 'json'
+
+    url = 'https://api.github.com/gists/public?page=' + rand(10).to_s + '&per_page=1'
+
+    res = Net::HTTP.get(URI.parse(url))
+    hash = JSON.parse(res)[0]
+    key_name = hash["files"].keys.first
+    raw_url = hash["files"][key_name]["raw_url"]
+
+    @@lang = hash["files"][key_name]["language"]
+    @code = Net::HTTP.get(URI.parse(raw_url))
   end
 
   def judge
-    if params[:answer] == 'JavaScript'
-      flash[:notice] = "正解です"
+    if params[:answer] == @@lang
+      flash[:notice] = "正解です！"
     else
-      flash[:notice] = "チガウヨ"
+      flash[:notice] = "正解は#{@@lang}でした！"
     end
     redirect_to("/quiz")
   end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_22_081136) do
+ActiveRecord::Schema.define(version: 2019_10_29_060024) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_10_22_081136) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password"
+    t.integer "score"
   end
 
 end
